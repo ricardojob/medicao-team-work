bf6c9c5af4840c77b93772b6967ea31409d54aee
Ricardo Job <sousajob@gmail.com>
2022-11-18 14:59:37 -0300

    fix code test

--- src/tests/br/ufmg/engsoft/reprova/tests/database/CoarseGrainedCourseDAOTest.java
+++ src/tests/br/ufmg/engsoft/reprova/tests/database/CoarseGrainedCourseDAOTest.java
@@ -1,49 +1,33 @@
 package br.ufmg.engsoft.reprova.tests.database;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.*;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import br.ufmg.engsoft.reprova.database.CoarseGrainedCourseDAO;
-import br.ufmg.engsoft.reprova.database.Mongo;
-import br.ufmg.engsoft.reprova.mime.json.Json;
 import br.ufmg.engsoft.reprova.model.CoarseGrainedCourse;
 import br.ufmg.engsoft.reprova.model.Course;
 import br.ufmg.engsoft.reprova.model.CourseFactory;
-import br.ufmg.engsoft.reprova.model.Student;
 import br.ufmg.engsoft.reprova.model.variability.CoarseGrainedCourseFactory;
 
-public class CoarseGrainedCourseDAOTest {
-	private static final Logger logger = LoggerFactory.getLogger(CoarseGrainedCourseDAOTest.class);
-	private static Mongo db;
-	private static Json json;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoarseGrainedCourseDAOTest extends BaseTest {
 	private static CoarseGrainedCourseDAO dao;
-	@BeforeAll
-    public static void setup() {
-		logger.info("Starting DB connection");
-		try {
-			db = new Mongo("reprova");
-			json = new Json();
+    @BeforeEach
+    public  void setup() {
+//		logger.info("Starting DB connection");
+//		try {
 			dao = new CoarseGrainedCourseDAO(db, json);
-		}
-		catch(Exception e){
-			logger.info("Could not connect to mongoDB");
-			db = null;
-		}
-		
+//		}
+//		catch(Exception e){
+//			logger.info("Could not connect to mongoDB");
+//			db = null;
+//		}
+
     }
-	@BeforeEach
-	void assumeConnection() {
-		assumeFalse(db == null);
-	}
-	
+
 	@Test
 	void test_insertion() {
 		CourseFactory factory = new CoarseGrainedCourseFactory();
@@ -57,10 +41,10 @@ public class CoarseGrainedCourseDAOTest {
         CoarseGrainedCourse course = (CoarseGrainedCourse)factory.createCourse(2019, Course.Reference._1, "test_retrieval", 50.0f);
         dao.add(course);
         Course course2 = dao.get(course);
-        Assertions.assertEquals(course.getScore(),course2.getScore());
-        Assertions.assertEquals(course.year,course2.year);
-        Assertions.assertEquals(course.ref.value,course2.ref.value);
-        Assertions.assertEquals(course.courseName,course2.courseName);
+        assertEquals(course.getScore(),course2.getScore());
+        assertEquals(course.year,course2.year);
+        assertEquals(course.ref.value,course2.ref.value);
+        assertEquals(course.courseName,course2.courseName);
         
 	}
 	
--- src/tests/br/ufmg/engsoft/reprova/tests/database/FineGrainedCourseDAOTest.java
+++ src/tests/br/ufmg/engsoft/reprova/tests/database/FineGrainedCourseDAOTest.java
@@ -1,51 +1,48 @@
 package br.ufmg.engsoft.reprova.tests.database;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.*;
 
 import java.util.Arrays;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.*;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 import br.ufmg.engsoft.reprova.database.FineGrainedCourseDAO;
-import br.ufmg.engsoft.reprova.database.Mongo;
-import br.ufmg.engsoft.reprova.mime.json.Json;
+//import br.ufmg.engsoft.reprova.database.Mongo;
+//import br.ufmg.engsoft.reprova.mime.json.Json;
 import br.ufmg.engsoft.reprova.model.FineGrainedCourse;
 import br.ufmg.engsoft.reprova.model.Course;
 import br.ufmg.engsoft.reprova.model.CourseFactory;
 import br.ufmg.engsoft.reprova.model.Student;
 import br.ufmg.engsoft.reprova.model.variability.FineGrainedCourseFactory;
 
-public class FineGrainedCourseDAOTest {
-	private static final Logger logger = LoggerFactory.getLogger(FineGrainedCourseDAOTest.class);
-	private static Mongo db;
-	private static Json json;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FineGrainedCourseDAOTest extends BaseTest {
+//	private static final Logger logger = LoggerFactory.getLogger(FineGrainedCourseDAOTest.class);
+//	private static Mongo db;
+//	private static Json json;
 	private static FineGrainedCourseDAO dao;
-	@BeforeAll
-    public static void setup() {
-		logger.info("Starting DB connection");
-		try {
-			db = new Mongo("reprova");
-			json = new Json();
+	@BeforeEach
+    public  void setup() {
+//		logger.info("Starting DB connection");
+//		try {
+//			db = new Mongo("reprova");
+//			json = new Json();
 			dao = new FineGrainedCourseDAO(db, json);
-		}
-		catch(Exception e){
-			logger.info("Could not connect to mongoDB");
-			db = null;
-		}
+//		}
+//		catch(Exception e){
+//			logger.info("Could not connect to mongoDB");
+//			db = null;
+//		}
 		
     }
-	@BeforeEach
-	void assumeConnection() {
-		assumeFalse(db == null);
-	}
+//	@BeforeEach
+//	void assumeConnection() {
+//		assumeFalse(db == null);
+//	}
 	
 	@Disabled
 	@Test
@@ -66,10 +63,10 @@ public class FineGrainedCourseDAOTest {
         FineGrainedCourse course = (FineGrainedCourse)factory.createCourse(2019, Course.Reference._1, "test_retrieval", 50.0f);
         dao.add(course);
         Course course2 = dao.get(course);
-        Assertions.assertEquals(course.getScore(),course2.getScore());
-        Assertions.assertEquals(course.year,course2.year);
-        Assertions.assertEquals(course.ref.value,course2.ref.value);
-        Assertions.assertEquals(course.courseName,course2.courseName);
+        assertEquals(course.getScore(),course2.getScore());
+        assertEquals(course.year,course2.year);
+        assertEquals(course.ref.value,course2.ref.value);
+        assertEquals(course.courseName,course2.courseName);
         
 	}
 	
